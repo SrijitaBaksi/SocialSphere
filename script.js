e252f1efed808533cd326d868d8cb91b89bb596f
SrijitaBaksi <baksisrijita@gmail.com>
2024-05-13 01:48:33 +0530

    added dropdown

---
+++ script.js
@@ -0,0 +1,11 @@
+let profile_icon = document.querySelector(".nav-user-icon")
+let settings_menu = document.querySelector(".settings-menu")
+let dark_btn = document.getElementById("dark-btn")
+
+profile_icon.onclick=function(){
+    settings_menu.classList.toggle("hide")
+}
+
+dark_btn.onclick = function(){
+    dark_btn.classList.toggle("dark-btn-on")
+}
