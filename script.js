010a9b634e679d93aa84f634693b75ef42bf115a
SrijitaBaksi <baksisrijita@gmail.com>
2024-05-13 02:45:49 +0530

    local storage used for retaining theme

--- script.js
+++ script.js
@@ -9,4 +9,24 @@ profile_icon.onclick=function(){
 dark_btn.onclick = function(){
     dark_btn.classList.toggle("dark-btn-on")
     document.body.classList.toggle("dark-theme")
+    if(localStorage.getItem("theme")=="light"){
+        localStorage.setItem("theme","dark")
+    }
+    else{
+        localStorage.setItem("theme","light")
+    }
+}
+localStorage.setItem("theme","dark")
+localStorage.getItem("theme")
+
+if(localStorage.getItem("theme")=="light"){
+    dark_btn.classList.remove("dark-btn-on")
+    document.body.classList.remove("dark-theme")
+}
+else if(localStorage.getItem("theme")=="dark"){
+    dark_btn.classList.add("dark-btn-on")
+    document.body.classList.add("dark-theme")
+}
+else{
+    localStorage.setItem("theme","light")
 }
